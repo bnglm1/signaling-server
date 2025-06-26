c347c2748574ebac53322b91ec6c6794cf2929ef
bnglm1 <137788571+bnglm1@users.noreply.github.com>
2025-06-26 09:54:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -50,6 +50,8 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('🚀 Signaling sunucusu çalışıyor: http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`🚀 Signaling sunucusu çalışıyor: http://localhost:${PORT}`);
 });
